3bba8b0d8647880cdb603106a06795e15cd0c094
Admin <User@Lions-Mac.local>
2013-04-21 20:22:00 +0300

    Initial Commit

---
+++ DeVega/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DeVega
+//
+//  Created by Admin on 21/04/2013.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeVega/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DeVega
+//
+//  Created by Admin on 21/04/2013.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
